2dea8646b17bfd9fb4251009d54b09a793014a51
aneesh2308 <aneeshsrivastava2000@gmail.com>
2022-12-16 13:26:41 +0530

    Second Commit

---
+++ src/style.js
@@ -0,0 +1,11 @@
+const style = {
+    App:{
+        maindivtag:{ 
+            display:'flex', 
+            justifyContent:'center', 
+            alignItems:'center', 
+            height: '100vh' 
+        }
+    }
+}
+export default style; 
